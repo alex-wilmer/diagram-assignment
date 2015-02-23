24ae4749f391a44bcb70c887398f080d8021b246
Alex Wilmer <djazium@gmail.com>
2015-02-23 03:03:51 -0500

    only retrieve images from other group, mongo dot notation for find()

--- server/startup.js
+++ server/startup.js
@@ -1,6 +1,9 @@
 Meteor.startup(function () {
 
-  var admin = Meteor.users.findOne({username:'admin'});
+  var admin = Meteor.users.findOne({username:'admin'})
+    , deadline = 
+        moment('2015 10 25 5 30', 'YYYY MM DD H mm')
+          .format('M/D/YYYY H:mm A');
 
   if (!admin) {
 
@@ -24,33 +27,34 @@ Meteor.startup(function () {
     });
   }
 
-  var deadline = 
-    moment('2015 10 25 5 30', 'YYYY MM DD H mm').format('M/D/YYYY H:mm A');
-
-  var flipper = false;
-
   Meteor.methods({
     getDeadline: function() {
       return deadline;
     }
-  , activateDeadline: function() {
-
-      var admin = Meteor.users.findOne({
-        username: 'admin'
-      });
-      
+  , activateDeadline: function() { 
+      var admin = Meteor.users.findOne({username:'admin'})
       if ((admin && admin._id) === this.userId) {
         var users = Meteor.users.find({
-          profile: {
-            submitted: true
-          }
+          'profile.submitted': true
         })
-        .forEach(function(user) {
-          var group = (flipper = !flipper) ? 'A' : 'B';
-          Meteor.users.update(user._id, {
+        .forEach(function(user, index) {
+          var group = (index % 2 === 0) ? 'A' : 'B'
+            , newProperties = {
+                group: group
+              , profile: {
+                  group: group
+                , submitted: user.profile.submitted
+                }
+              };
+
+          Images.update({userId: user._id}, {
             $set: {
               group: group
             }
+          });     
+
+          Meteor.users.update(user._id, {
+            $set: newProperties 
           });
         });
 
